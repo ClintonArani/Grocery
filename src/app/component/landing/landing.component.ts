d1617a34ee0a932c642ff99b2d967956dcea31a5
Janathan <jones@gmail.com>
2024-10-17 12:13:51 +0300

    completed navbar

--- src/app/component/landing/landing.component.ts
+++ src/app/component/landing/landing.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faFacebook, faTwitter, faLinkedin, faPinterest } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faUser, faPhone, faHeart, faChevronDown, faShoppingBag, faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule, CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
+  encapsulation: ViewEncapsulation.None
 })
 export class NavbarComponent {
-
+  faFacebook = faFacebook;
+  faTwitter = faTwitter;
+  faLinkedin = faLinkedin;
+  faPinterest = faPinterest;
+  faEnvelope = faEnvelope;
+  faUser = faUser;
+  faPhone = faPhone;
+  faHeart = faHeart;
+  faChevronDown = faChevronDown;
+  faShoppingBag = faShoppingBag;
+  faShoppingBasket = faShoppingBasket;
+  orderAmount: number = 99;
+  itemAmount: number = 150;
 }
