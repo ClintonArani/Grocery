4491e60048f6ceb027cd791dfcbbb66029d5911d
Clinton Arani <clintonarani420@gmail.com>
2024-10-21 11:27:32 +0300

    Designing of products

--- src/app/component/landing/landing.component.ts
+++ src/app/component/landing/landing.component.ts
@@ -1,19 +1,34 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBars, faChevronDown, faPhone } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [NavbarComponent, CommonModule],
+  imports: [NavbarComponent, CommonModule, FontAwesomeModule],
   templateUrl: './landing.component.html',
-  styleUrl: './landing.component.css'
+  styleUrls: ['./landing.component.css']
 })
 export class LandingComponent {
-  isDropdownOpen: boolean = false;
+  faBars = faBars
+  faChevronDown = faChevronDown
+  isDropdownOpen: boolean = true;
+  faPhone = faPhone;
 
-  //toggle the dropdown visibility
-  toggleDropdown(): void{
-    this.isDropdownOpen = !this.isDropdownOpen;
+  // Toggle the dropdown visibility
+  toggleDropdown(): void {
+    this.isDropdownOpen = !this.isDropdownOpen; // Toggle the value between true and false
   }
+  items = [
+    {name: "Fresh Fruit", image: "../assets/fresh_fruit.png"},
+    {name: "Dried Fruit", image: "../assets/dried_fruit.png"},
+    {name: "Vegetables", image: "../assets/vegetables.png"},
+    {name: "Drink Fruit", image: "../assets/drink_fruit.png"},
+    {name: "Apple Fruit", image: "../assets/apples.png"},
+  ]
+ 
 }
+
