9af7394d8b7dd86976ea49db50e277e7517d49c1
Clinton Arani <clintonarani420@gmail.com>
2024-10-23 22:41:13 +0300

    completed designing the landing page of Grocery Ecommerce

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './component/landing/landing.component';
+import { LoginComponent } from './component/login/login.component';
 
 export const routes: Routes = [
     { path: '', pathMatch: 'full', component: LandingComponent },
-    {path: 'homepage',component: LandingComponent}
+    {path: 'homepage',component: LandingComponent},
+    {path: 'login',component:LoginComponent}
 ];
